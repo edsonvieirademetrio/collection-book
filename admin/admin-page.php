2ac5f8d8e0c298a4f738e48f961ad5c70f79c515
edsonvieirademetrio <edsonvieirademetrio@gmail.com>
2024-03-19 00:03:42 -0300

    Plugin settings page

---
+++ admin/admin-page.php
@@ -0,0 +1,28 @@
+<?php
+
+//Admin page Collections
+add_action('admin_menu', 'collection_admin_menu');
+
+function collection_admin_menu() {
+    add_menu_page(
+        'Controle Básico de Acervo', 
+        'Acervo', 
+        'manage_options', 
+        'collections-settings', 
+        'collection_settings_page' 
+    );
+}
+
+
+// Plugin Settings Page
+function collection_settings_page() {
+    // Check permissions user
+    if (!current_user_can('manage_options')) {
+        wp_die(__('Você não tem permissão para acessar esta página.'));
+    }
+
+    // Content
+    echo '<div class="wrap">';
+    include_once('collection-book-admin-page.php');
+    echo '</div>';
+}
