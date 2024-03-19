ad1cd315ef12c1cdf5e71b889cd9f234dccd6b31
edsonvieirademetrio <edsonvieirademetrio@gmail.com>
2024-03-18 23:48:11 -0300

    initial structure

---
+++ collection-book.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Collection Book
+Description: Controle Básico de Acervos.
+Version: 1.0
+Author: Edson Vieira Demetrio
+*/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+//Register CPT and CF
+
+
+//API CRUD
+
+
+//Admin Page for Collections
